3a507109198ef3e834ec72a96a78c2dcb4bff1a3
Renato Gomes Dos Santos <renato.work.art@gmail.com>
2020-11-27 16:18:54 -0300

    update
    scroll reveal added

---
+++ js/dist/scrollreveal.dev.js
@@ -0,0 +1,23 @@
+"use strict";
+
+ScrollReveal({
+  duration: 500
+});
+ScrollReveal({
+  reset: true
+});
+ScrollReveal().reveal('.show-1', {
+  deley: 300
+});
+ScrollReveal().reveal('.show-2', {
+  delay: 400
+});
+ScrollReveal().reveal('.show-3', {
+  delay: 600
+});
+var slideUp = {
+  distance: '80px',
+  origin: 'bottom',
+  opacity: 0
+};
+ScrollReveal().reveal('.show', slideUp);
