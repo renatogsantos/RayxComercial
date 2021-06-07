4aa1f53e94a3f32a7d12c9dbf27275279a6833b1
Renato Gomes dos Santos <39703265+renatogsantos@users.noreply.github.com>
2021-06-07 15:56:54 -0300

    update
    update

--- js/search.js
+++ js/search.js
@@ -1,18 +1,16 @@
-function myFunction() {
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[2];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }       
-    }
+function filterTable(event) {
+  var filter = event.target.value.toUpperCase();
+  var rows = document.querySelector("#myTable tbody").rows;
+  
+  for (var i = 0; i < rows.length; i++) {
+      var firstCol = rows[i].cells[1].textContent.toUpperCase();
+      var secondCol = rows[i].cells[2].textContent.toUpperCase();
+      if (firstCol.indexOf(filter) > -1 || secondCol.indexOf(filter) > -1) {
+          rows[i].style.display = "";
+      } else {
+          rows[i].style.display = "none";
+      }      
   }
+}
+
+document.querySelector('#myInput').addEventListener('keyup', filterTable, false);
