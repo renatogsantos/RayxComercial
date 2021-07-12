595cb65bece88114cf9ffaf5e02e1815c48fa232
Renato Gomes dos Santos <39703265+renatogsantos@users.noreply.github.com>
2021-07-12 14:16:09 -0300

    update
    password add

--- js/style.js
+++ js/style.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 //pede senha para acessar a página de blog
 function loginAcess() {
 	var login = prompt("Por favor digite a senha de acesso: (Para acessar a página de introdução aos produtos Rayx Brasil, solicite o acesso com um de nossos colaboradores na página de contatos Rayx.com.br)")
-	if (login == 32272538) {
+	if (login == "rayx2021") {
 		alert("Senha correta!")
 	} else {
 		window.location.href = "https://www.rayx.com.br";
