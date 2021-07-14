0c0769c8d67d9d86ca3d79b7fd35fb20b2248daf
Renato Gomes dos Santos <39703265+renatogsantos@users.noreply.github.com>
2021-07-14 09:57:41 -0300

    update
    zeroCol to search

--- js/dist/search.dev.js
+++ js/dist/search.dev.js
@@ -5,10 +5,11 @@ function filterTable(event) {
   var rows = document.querySelector("#myTable tbody").rows;
 
   for (var i = 0; i < rows.length; i++) {
+    var zeroCol = rows[i].cells[0].textContent.toUpperCase();
     var firstCol = rows[i].cells[1].textContent.toUpperCase();
     var secondCol = rows[i].cells[2].textContent.toUpperCase();
 
-    if (firstCol.indexOf(filter) > -1 || secondCol.indexOf(filter) > -1) {
+    if (zeroCol.indexOf(filter) > -1 || firstCol.indexOf(filter) > -1 || secondCol.indexOf(filter) > -1) {
       rows[i].style.display = "";
     } else {
       rows[i].style.display = "none";
